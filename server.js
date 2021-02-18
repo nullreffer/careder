3939e98d2b157370a54e5f83403976b12597d72e
Jay Desai <jadesai@microsoft.com>
2021-02-17 21:22:45 -0800

    Added boiler plate

---
+++ server.js
@@ -0,0 +1,62 @@
+// load express framework
+var bodyParser = require('body-parser')
+var express = require('express')
+var humanDate = require('human-date')
+var mongojs = require('mongojs')
+var urlEncoding = bodyParser.urlencoded({
+  extended: false
+})
+var app = express()
+var package = require('./package.json')
+
+var db = mongojs(process.env.CAREDER_DATABASE_URL)
+var submissions = db.collection('submissions')
+
+// set render engine
+app.set('view engine', 'pug')
+
+app.use(express.static('static/'))
+
+// handle post submissio
+app.post('/submit', urlEncoding, function (req, res) {
+  if (req.body.message === '' || req.body.author === '') {
+    res.redirect('/')
+    return
+  }
+
+  // insert submission into database
+  submissions.insert({
+    message: req.body.message,
+    author: req.body.author,
+    date: humanDate.prettyPrint(new Date())
+  }, function (err) {
+    if (err != null) {
+      res.write('could not save submission. please try again later.')
+    } else {
+      res.redirect('/')
+    }
+  })
+})
+
+// handle guestbook listing
+app.get('/', function (req, res) {
+  submissions.find(function (err, docs) {
+    if (err != null) {
+      res.write('error while reading from database. please try again later.')
+      console.log(err)
+    } else {
+      res.render('index', {
+        title: 'Guestbook',
+        version: package.version,
+        submissions: docs
+      })
+    }
+  })
+})
+
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Server listening on http://%s:%s', host, port)
+})
